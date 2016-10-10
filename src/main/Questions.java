3bedb87c73d28ba822a1ad5db98f9de9bc079bad
Ayush Goyal <ayushgoyal09@gmail.com>
2016-10-10 15:37:50 -0700

    bean for questions

---
+++ src/main/Questions.java
@@ -0,0 +1,49 @@
+package main;
+
+public class Questions {
+	String strandId;
+	String strandName;
+	String standardId;
+	String standardName;
+	String questionId;
+	String difficulty;
+	public String getStrandId() {
+		return strandId;
+	}
+	public void setStrandId(String strandId) {
+		this.strandId = strandId;
+	}
+	public String getStrandName() {
+		return strandName;
+	}
+	public void setStrandName(String strandName) {
+		this.strandName = strandName;
+	}
+	public String getStandardId() {
+		return standardId;
+	}
+	public void setStandardId(String standardId) {
+		this.standardId = standardId;
+	}
+	public String getStandardName() {
+		return standardName;
+	}
+	public void setStandardName(String standardName) {
+		this.standardName = standardName;
+	}
+	public String getQuestionId() {
+		return questionId;
+	}
+	public void setQuestionId(String questionId) {
+		this.questionId = questionId;
+	}
+	public String getDifficulty() {
+		return difficulty;
+	}
+	public void setDifficulty(String difficulty) {
+		this.difficulty = difficulty;
+	}
+
+	
+
+}
